b728e8588f229347260fb1f509d6b4b63cde3c56
Rhoxio <rhoxiodbc@gmail.com>
2020-07-01 03:52:33 -0700

    got some pieces of the status duration logic and flow in

--- lib/adjective/status.rb
+++ lib/adjective/status.rb
@@ -1,48 +1,68 @@
 module Adjective
-  class Status
-    attr_reader :name, :duration, :initialized_at, :affected_attributes, :modifiers
-    attr_accessor :remaining
+  module Status
 
-    def initialize(name, opts = {})
+    # Status is different from something like an attack in that it applies
+    # to things that afflict the actor for more than one turn.
+    # An attack would be instant the moment it resolves, but can leave a status effect.
+
+    # What if the status is applied initially and can stack higher than initial application? (#DONE)
+
+    def initialize_status(name, opts = {})
       @name = name || nil
+  
       @duration = opts[:duration] ||= 0
-      @remaining = opts[:remaining] ||= @duration
+      @max_duration = opts[:max_duration] ||= opts[:duration]
+      @remaining_duration = opts[:remaining_duration] ||= opts[:duration]
+
       @initialized_at = Time.now
+
       @modifiers = opts[:affected_attributes]
       @affected_attributes = opts[:affected_attributes].map{|entry| entry[0] }
+
+      self.class.send(:attr_accessor, :remaining_duration)
+      [:name, :duration, :initialized_at, :affected_attributes, :modifiers].each do |attribute| 
+        self.class.send(:attr_reader, attribute)
+      end
+      
+      #converts the attributes over to instance variable form (@thing) for easier consumption elsewhere
       convert_attributes
     end
 
     # Tick functionality
 
     def tick(&block)
+      # Reduce duration
+      # Allow for block to modify
+      # return list of things ticked so they can be applied with Statusable
       # This method is meant to be overridden if you want other 'tick' functionality.
-      if block_given?
+      if block_given? 
         yield(self) 
       else
-        @duration += 1
+        # Default functionality.
+        @duration -= 1
       end
-      return expired? ? false : true
+      return self
     end
 
-    # Utility methods
+    def extend_by(extension)
+      @remaining_duration += extension
+      normalize_remaining_duration
+    end    
+
+    def normalize_remaining_duration
+      @remaining_duration = @max_duration if @remaining_duration > @max_duration
+      @remaining_duration = 0 if @remaining_duration < 0
+    end    
+
+    # # Utility methods
 
     def expired?
-      @remaining == 0
+      @remaining_duration == 0
     end
 
     def expiring?
       # This method seems like a meme, but I think it makes sense
-      @remaining == 1
-    end
-
-    def instant?
-      # If the duration is 0, it should apply immediately for a single turn.
-      @duration == 0
-    end
-
-    def over_time?
-      @duration > 1 || @duration == :unlimited
+      @remaining_duration == 1
     end
 
     private
---
+++ spec/surrogate_classes/surrogate_status_heal.rb
@@ -0,0 +1,7 @@
+# This is meant to represent a child class once Item is inherited from it.
+class SurrogateStatusHeal
+
+  def initialize
+
+  end
+end
