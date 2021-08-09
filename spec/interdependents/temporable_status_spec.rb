c2051ac12b30f008844ce044780aa6670987c214
Rhoxio <rhoxiodbc@gmail.com>
2021-08-09 01:55:21 -0700

    got temporable status tests back up and running

--- spec/interdependents/temporable_status_spec.rb
+++ spec/interdependents/temporable_status_spec.rb
@@ -1,41 +1,45 @@
-# RSpec.describe "Temporable integration with Status" do
-#   before(:example) do 
-#     @renew = SurrogateStatus.new("Renew", {modifiers: { hitpoints: 3}, max_duration: 5})
-#     @agony = SurrogateStatus.new("Agony", {modifiers: { hitpoints: -3}, max_duration: 10})
-#     @rend = SurrogateStatus.new("Rend", {modifiers: { hitpoints: 1}})
-#   end
+RSpec.describe "Temporable integration with Status" do
+  before(:example) do 
+    @renew_effect = Adjective::Modifier.new("RenewHealing", {hitpoints: 3})
+    @agony_effect = Adjective::Modifier.new("AgonyEffect", {hitpoints: -3})
+    @rend_effect = Adjective::Modifier.new("RendEffect", { hitpoints: -1 })
 
-#   describe "when a status initializes" do 
-#     it "will set duration values correctly" do 
-#       expect(@renew.max_duration).to eq(5)
-#       expect(@renew.remaining_duration).to eq(5)
-#     end
+    @renew = SurrogateStatus.new("Renew", { modifiers: [@renew_effect], max_duration: 5 })
+    @agony = SurrogateStatus.new("Agony", {modifiers: [@agony_effect], max_duration: 10})    
+    @rend = SurrogateStatus.new("Rend", {modifiers: [@rend_effect], tick_type: :linear})
+  end
 
-#     it "will default to 1 if no initial duration is set" do
-#       expect(@rend.max_duration).to eq(1)
-#       expect(@rend.remaining_duration).to eq(1)
-#     end
+  describe "when a status initializes" do 
+    it "will set duration values correctly" do 
+      expect(@renew.max_duration).to eq(5)
+      expect(@renew.remaining_duration).to eq(5)
+    end
 
-#     it "will throw an error if provided @remaining_duration is larger than @max_duration" do
-#       expect{SurrogateStatus.new("Deadly Poison", {modifiers: { hitpoints: -2}, remaining_duration: 11, max_duration: 10})}
-#       .to raise_error(ArgumentError)
-#     end    
-#   end
+    it "will default to 1 if no initial duration is set" do
+      expect(@rend.max_duration).to eq(1)
+      expect(@rend.remaining_duration).to eq(1)
+    end
 
-#   describe "when utility methods are called" do 
-#     it "#expired? will correctly tell if a status is expired" do 
-#       # Assumes default of 1 is present.
-#       @rend.remaining_duration -= 1
-#       expect(@rend.expired?).to eq(true)
-#     end
+    it "will throw an error if provided @remaining_duration is larger than @max_duration" do
+      expect{SurrogateStatus.new("Deadly Poison", {modifiers: { hitpoints: -2}, remaining_duration: 11, max_duration: 10})}
+      .to raise_error(ArgumentError)
+    end    
+  end
 
-#     it "#expiring? will correctly tell if a status is about to expire" do 
-#       expect(@rend.expiring?).to eq(true)
-#     end
+  describe "when utility methods are called" do 
+    it "#expired? will correctly tell if a status is expired" do 
+      # Assumes default of 1 is present.
+      @rend.remaining_duration -= 1
+      expect(@rend.expired?).to eq(true)
+    end
 
-#     it "#max_duration? will correctly tell if the status is at max duration" do 
-#       expect(@rend.max_duration?).to eq(true)
-#     end
-#   end 
+    it "#expiring? will correctly tell if a status is about to expire" do 
+      expect(@rend.expiring?).to eq(true)
+    end
 
-# end
+    it "#max_duration? will correctly tell if the status is at max duration" do 
+      expect(@rend.max_duration?).to eq(true)
+    end
+  end 
+
+end
